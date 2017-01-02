c663af0fcc070809c1c229cec71a318fbb55dbe3
GaganGupta <ggupta19693@gmail.com>
2017-01-03 00:11:34 +0530

    all insertion operations

--- Linked List/linked_list.c
+++ Linked List/linked_list.c
@@ -8,8 +8,8 @@ struct node {
 //for creation
 struct node* createnode(int key){
 	struct node *newnode = (struct node*) malloc(sizeof(struct node));
-	newnode->key = key;
-	newnode->next = NULL;
+	newnode -> key = key;
+	newnode -> next = NULL;
 	return newnode;
 }
 
@@ -17,28 +17,53 @@ struct node* createnode(int key){
 void display(struct node* temp){
 	struct node* head = temp;
 	while(head != NULL){
-		printf("%d\n", head->key);
-		head = head->next;
+		printf("%d\n", head -> key);
+		head = head -> next;
 	}
 }
 
 //for insertion at the end
-int insertion(struct node** head, int key){
+int insertion(struct node** head, int key, int position){
+	struct node *tempnode = createnode(key);
 	if(*head == NULL){
-		*head = createnode(key);
+		*head = tempnode;
 		return 1;
 	}
-	else
-		insertion(&(*head)->next, key);
+	else if(position == 0){
+		tempnode -> next = *head;
+		*head = tempnode;
+		return 1;
+	}
+	else if(position == -1){
+		while((*head) -> next != NULL)
+			*head = (*head) -> next;
+		(*head) -> next = tempnode;
+		return 1;
+	}
+	else{
+		int count = 1;
+		while((*head) -> next != NULL && count != position){
+			count++;
+			*head = (*head) -> next;
+		}
+		if((*head) -> next == NULL)
+			(*head) -> next = tempnode;
+		else{
+			struct node *temp = (*head) -> next;
+			(*head) -> next = tempnode;
+			tempnode -> next = temp;
+			return 1;
+		}
+	}
 }
 
 int main(){
 	struct node *head = NULL;
 	
 	while(1){
-		int choice, value, result;
+		int choice, value, result, position;
 
-		printf("\n1.Insertion\n2.Traversal\n3.Exit\nEnter : ");
+		printf("\n1.Insertion at end\n2.Insertion at front\n3.Insertion at a specific location\n4.Traversal\n5.Exit\nEnter : ");
 		scanf("%d", &choice);
 		
 		switch(choice){
@@ -46,16 +71,31 @@ int main(){
 			case 1: 
 			printf("\nEnter value : ");
 			scanf("%d",&value);
-			if(result = insertion(&head,value))
+			if(result = insertion(&head, value, 0))
 				printf("\nSuccess");
 			break;
 
 			case 2:
-			display(head);
-
+			printf("\nEnter value : ");
+			scanf("%d",&value);
+			if(result = insertion(&head, value, -1))
+				printf("\nSuccess");
 			break;
 
 			case 3:
+			printf("\nEnter value : ");
+			scanf("%d",&value);
+			printf("\nEnter position : ");
+			scanf("%d",&position);
+			if(result = insertion(&head, value, position))
+				printf("\nSuccess");
+			break;
+			
+			case 4:
+			display(head);
+			break;
+
+			case 5:
 			return -1;
 			break;
 		}
