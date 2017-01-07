1c58303fb9b3ef0a0e939a02c75d38e3fe6420ee
GaganGupta <ggupta19693@gmail.com>
2017-01-08 00:13:55 +0530

    stack implemented

--- Stack/stack.c
+++ Stack/stack.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "linked_list.h"
-#define LIMIT 5
 
 int is_empty(struct node* head){
-	head == NULL ? return 1 : return -1;
+	if (head == NULL) return 1; return -1;
 }
 
-int is_full(int count){
-	count == LIMIT ? return 1 : return -1;
+int is_full(int count, int limit){
+	if (count == limit) return 1; else return -1;
 }
 
-int push(struct node** head, int key, int count){
-	is_full(count) && insertion(head, key, 0) ? return 1 : return -1;
+int push(struct node** head, int key){
+	if (insertion(head, key, 0)) return 1; else return -1;
 }
 
 int pop(struct node** head){
-	is_empty(count) && deletion(head, 0) ? return 1 : return -1;
+	if (deletion(head, 0)) return 1; else return -1;
 }
 
 int main(){
 
 	struct node* head = NULL;
-	int count,choice,value;
-	count = choice = value;
+	int count, choice, value, size;
+	count = choice = value = 0;
+
+	printf("\nEnter the size of the stack : ");
+	scanf("%d", &size);
 
 	while(1){
 
@@ -35,7 +37,8 @@ int main(){
 			case 1	:
 			printf("\nEnter value : ");
 			scanf("%d", &value);
-			push(&head, value, count) ? printf("\nSuccess\nLIMIT : %d, CURRENT : %d", LIMIT, count++) : printf("\nFailed");	
+			is_full(count, size) ? printf("yes") : printf("no");
+			push(&head, value) ? printf("\nSuccess\nLIMIT : %d, CURRENT : %d", size, ++count) : printf("\nFailed");	
 			break;
 			
 			case 2	:
