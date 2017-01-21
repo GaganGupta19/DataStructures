43a1f7faa61b45b11a0cb967f60e03a4a7f0327e
Gagan Gupta <gagan_gupta@live.in>
2017-01-22 00:25:30 +0530

    implemented solution for this problem

--- Linked List/separate_even_odd.c
+++ Linked List/separate_even_odd.c
@@ -4,10 +4,40 @@
 
 // Let the list be 1-2-3-4-5-6-7-8-9-10-11
 
+int separate_even_odds(struct node* head){
+	struct node *even_list, *odd_list, *even_end, *odd_end, *temp;
+	even_list = odd_list = even_end = odd_end = NULL;
+	temp = head;
+	if(head == NULL)
+		return -1;
+	else{
+		while(temp){
+			if(temp -> key % 2 == 0){
+				if(even_list == NULL)
+					even_list = even_end = temp;
+				else{
+					even_end -> next = temp;
+					even_end = temp;	
+				}
+			}
+			else{
+				if(odd_list == NULL)
+					odd_list = odd_end = temp;
+				else{
+					odd_end -> next = temp;
+					odd_end = temp;	
+				}
+			}
+		}	
+	}
+	display(head);
+	return 1;
+}
+
 int main(){
 
 	struct node *head;
-	
+
 	head = createnode(1);
 	head -> next = createnode(2);
 	head -> next -> next = createnode(3);
@@ -18,6 +48,10 @@ int main(){
 	head -> next -> next -> next -> next -> next -> next -> next = createnode(8);
 	head -> next -> next -> next -> next -> next -> next -> next -> next = createnode(9);
 	head -> next -> next -> next -> next -> next -> next -> next -> next -> next = createnode(10);
+	head -> next -> next -> next -> next -> next -> next -> next -> next -> next -> next = createnode(11);
+
+	if(separate_even_odds(head) == -1) printf("\nNULL node");
+	free(head);
 	return 0;	
 }
 
