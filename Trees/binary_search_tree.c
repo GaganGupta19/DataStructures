334464a2415acdf70d06dc2cf8babe8a9dce8e35
Gagan Gupta <gagan_gupta@live.in>
2017-01-27 00:57:36 +0530

    Inserstion in BST complete

--- Trees/binary_search_tree.c
+++ Trees/binary_search_tree.c
@@ -12,57 +12,78 @@ struct node* createnode(int key){
 	newnode -> left = newnode -> right = NULL;
 }
 
-int insert(struct node *root, int key){
-	if(root == NULL){
-		root = createnode(key);
-		return 1;
-	}
-	else if(root -> key > key ){
-		insert(root->left, key);
-	}
-	else if(root -> key < key ){
-		insert(root -> right, key);
-	}
-	return -1;
+struct node* insert(struct node *root, int key){
+
+    if (root == NULL) return createnode(key);
+
+    if (key < root->key)
+        root->left  = insert(root->left, key);
+    else if (key > root->key)
+        root->right = insert(root->right, key);   
+    
+    return root;
 }
 
 void preorder(struct node* root){
 
 	if(root){
-		printf("\n%d", root->key);
-		preorder(root->left);
-		preorder(root->right);
+		printf("%d->", root->key);
+		preorder(root -> left);
+		preorder(root -> right);
 	}
 }
 
 void postorder(struct node* root){
 	if(root){
-		preorder(root->left);
-		preorder(root->right);
-		printf("\n%d", root->key);
+		postorder(root -> left);
+		postorder(root -> right);
+		printf("%d->", root->key);
+	}
+}
+
+void inorder(struct node* root){
+	if(root){
+		inorder(root -> left);
+		printf("%d->", root->key);
+		inorder(root -> right);
 	}
 }
 
 int main(){
-	struct node* root = NULL;
+	struct node* root = NULL, *temp;
 	int choice, value;
 
+	printf("\nEnter the root node : ");
+	scanf("%d", &value);
+	root = insert(root, value);
+
 	while(1){
-		printf("\n1.Insert\n2.Preorder\n3.Postorder\n4.Exit\nEnter : ");
+		printf("\n1.Insert\n2.Preorder\n3.Postorder\n4.Inorder\n5.All Traversals\n6.Exit\nEnter : ");
 		scanf("%d", &choice);
 		switch(choice){
 			case 1:
 				printf("\nEnter the value : ");
 				scanf("%d", &value);
-				insert(root, value) == 1 ? printf("\nSuccess") : printf("\nFailed");
+				(temp = insert(root, value)) != NULL ? printf("\nInserted : %d", temp -> key) : printf("\nFailed");
 				break;
 			case 2:
+				printf("\n");
 				preorder(root);
 				break;
 			case 3:
+				printf("\n");
 				postorder(root);
 				break;
 			case 4:
+				printf("\n");
+				inorder(root);
+				break;
+			case 5:
+				printf("\nPreorder : "); preorder(root);
+				printf("\nPostorder : "); postorder(root);
+				printf("\nInorder : "); inorder(root);
+				break;								
+			case 6:
 				return -1;
 				break;
 			default: 
